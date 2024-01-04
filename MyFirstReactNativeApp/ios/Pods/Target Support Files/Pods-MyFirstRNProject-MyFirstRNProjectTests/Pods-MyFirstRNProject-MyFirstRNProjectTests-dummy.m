e40283ffbc56eae988cb300b370e1f548ee6f810
Mahesha Piyumantha <75909711+mahesha-piyumantha@users.noreply.github.com>
2024-01-05 00:49:33 +0530

    First Commit removed

--- MyFirstReactNativeApp/ios/Pods/Target Support Files/Pods-MyFirstRNProject-MyFirstRNProjectTests/Pods-MyFirstRNProject-MyFirstRNProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyFirstRNProject_MyFirstRNProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_MyFirstRNProject_MyFirstRNProjectTests
-@end
--- MyFirstReactNativeApp/ios/Pods/Target Support Files/Pods-MyFirstRNProject/Pods-MyFirstRNProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyFirstRNProject : NSObject
-@end
-@implementation PodsDummy_Pods_MyFirstRNProject
-@end
